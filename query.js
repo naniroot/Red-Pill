aa7cdd56f4ea27e7af08344a40bc3b80cc3b0ab9
naniroot <narayanark17@gmail.com>
2013-03-04 20:35:55 -0500

    clean code

--- query.js
+++ query.js
@@ -1,5 +1,4 @@
 var redis = require('redis');
-//var flow = require('flow');
 var async = require('async');
 
 client = redis.createClient();
@@ -53,70 +52,21 @@ async.waterfall(
 
 */
 
-/*async.series({
-    one: function(callback){
-        setTimeout(function(){
-            callback(null, 1);
-        }, 200);
-    },
-    two: function(callback){
-        setTimeout(function(){
-            callback(null, 2);
-        }, 100);
-    }
-},
-function(err, results) {
-    // results is now equal to: {one: 1, two: 2}
-});
-*/
-
-
-
-
-
-
-
-/*async.series([
-
-client.smembers("AllIDs", getids)
-
-function getids(err,response){
-
-response.forEach(getObjects(e));
-
-}
-
-function getObjects(e){
-	console.log(e);
-}
-
-
-]);
-
-client.quit();
-*/
-
-
-/*async.waterfall([*/
-
 client.smembers("AllIDs", function(err, response){
 	if(err) console.log(err);
 	response.forEach(function(e){
 		console.log(e);
 		client1.smembers(e,function(err, respo){
 			if(err) console.log(err);
-			//console.log(respo);
 			respo.forEach(function(k){
 				console.log(k);
 				client2.hgetall(e+":"+k,function(err,res){
 					if(err) console.log(err);
 					console.log(res);
 				})		
-				//client2.quit();
 			})
 
 			client1.quit();	
-			//client2.quit();
 			
 		})
 
@@ -124,6 +74,5 @@ client.smembers("AllIDs", function(err, response){
 
 });
 client.quit();
-//]);
 
 
